0469ada5ea26b127a6293b850f32cf09af422cdc
Anant Mishra <anant.mishra.student@gmail.com>
2024-02-26 17:57:32 +0530

    add invite example for Javascript

---
+++ SendEmailInvite/js/invite.js
@@ -0,0 +1,66 @@
+const { Configuration, DefaultApi} = require('userservice-js-sdk/dist')
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+async function main() {
+    const smtpPassword = process.env.USERSERVICE_SMTP_PASSWORD
+    const smtpUsername = process.env.USERSERVICE_SMTP_SENDEREMAIL
+    const email = process.env.USERSERVICE_EMAIL
+    
+    const API_BASE_PATH = 'http://localhost:9001/api'
+    
+    const config = new Configuration({basePath: API_BASE_PATH})
+    const api = new DefaultApi(config)
+    
+    try {
+        const user = await api.createUser({
+            user: {
+                username: email,
+                attributes: {
+                    status: 'pending',
+                },
+            }
+        })
+
+        api.createEvent({
+            id: user.iD,
+            event: {
+                specversion: '1.0',
+                type: 'InviteUserEvent',
+                time: new Date(),
+                datacontenttype: 'application/json',
+                subject: user.iD,
+                id: '123',
+                data: user
+            }
+        })
+        
+        await sendEmail(email, smtpUsername, smtpPassword, 'Invite User', 'You are invited to userup.io')
+    } catch (err) {
+        console.error(`Error creating user: ${err}`)
+        process.exit(1)
+    }
+}
+
+async function sendEmail(toEmail, username, password, subject, body) {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: username,
+            pass: password,
+        },
+    })
+    
+    let info = await transporter.sendMail({
+        from: username,
+        to: toEmail,
+        subject: subject,
+        text: body,
+    })
+    
+    console.log('Message sent: %s', info.messageId)
+}
+
+main().catch(console.error)
